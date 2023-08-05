11ff0f53d8e375c5d877709e5ff4e6d781634fc9
Lautaro saez <Lautarosaez@gmail.com>
2023-08-05 12:53:53 -0300

    Se exporto la funcion sumar

--- sumar.js
+++ sumar.js
@@ -1,8 +1,8 @@
 /**
- * 
+ *
  * @param {*} a Primer parámetro de la suma
  * @param {*} b Segundo parámetro de la suma
- * @returns 
+ * @returns
  */
 
-const sumar = (a, b) => a + b;
+export const sumar = (a, b) => a + b;
