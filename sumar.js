97c9769f060dfd2f734e7f722556e3b3424a48fb
Lautaro saez <Lautarosaez@gmail.com>
2023-08-05 12:56:23 -0300

    Se cambio el nombre de sumar a add

--- sumar.js
+++ sumar.js
@@ -1,8 +1,8 @@
 /**
- * 
+ *
  * @param {*} a Primer parámetro de la suma
  * @param {*} b Segundo parámetro de la suma
- * @returns 
+ * @returns
  */
 
-const sumar = (a, b) => a + b;
+const add = (a, b) => a + b;
